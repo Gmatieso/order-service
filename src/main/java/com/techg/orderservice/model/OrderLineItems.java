6320f4ccd30fc8394ad41935c617e9861ec0e5a2
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-07 14:27:44 +0300

    OrderLineItems model done

--- src/main/java/com/techg/orderservice/model/OrderLineItems.java
+++ src/main/java/com/techg/orderservice/model/OrderLineItems.java
@@ -1,2 +1,28 @@
-package com.techg.orderservice.model;public class OrderLineItems {
+package com.techg.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name="t_order_line_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+
+
 }
