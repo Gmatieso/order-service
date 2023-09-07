747a93d7a6b09cf493b6493b4c07e8bd5d0b8eaa
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-07 14:27:01 +0300

    Order model done

--- src/main/java/com/techg/orderservice/model/Order.java
+++ src/main/java/com/techg/orderservice/model/Order.java
@@ -1,2 +1,25 @@
-package com.techg.orderservice.model;public class Order {
+package com.techg.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    //put your fields here
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
 }
