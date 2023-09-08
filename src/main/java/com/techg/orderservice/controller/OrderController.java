0afc660fe5ba769b16bbdd8ea42d2294f3b45a4a
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-08 13:35:04 +0300

    placeOrder Controller done

--- src/main/java/com/techg/orderservice/controller/OrderController.java
+++ src/main/java/com/techg/orderservice/controller/OrderController.java
@@ -1,2 +1,26 @@
-package com.techg.orderservice.controller;public class OrderController {
+package com.techg.orderservice.controller;
+
+import com.techg.orderservice.dto.OrderRequestDto;
+import com.techg.orderservice.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+//@RequiredArgsConstructor  this lombok annotation is a shorthand of manually creating a constructor
+public class OrderController {
+
+    //Inject your OrderService here
+    private  final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequestDto orderRequestDto){
+        orderService.placeOrder(orderRequestDto);
+        return "Order Placed Successfully";
+    }
 }
