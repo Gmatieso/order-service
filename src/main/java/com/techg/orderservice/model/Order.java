5e58258073a68fefba6d9c1075903e7a45955f20
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-08 13:51:26 +0300

    Order model done

--- src/main/java/com/techg/orderservice/model/Order.java
+++ src/main/java/com/techg/orderservice/model/Order.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 @Entity
 @Table(name="t_orders")
-@Getter
-@Setter
+//@Getter
+//@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
@@ -23,4 +23,28 @@ public class Order {
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<OrderLineItems> orderLineItemsList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public List<OrderLineItems> getOrderLineItemsList() {
+        return orderLineItemsList;
+    }
+
+    public void setOrderLineItemsList(List<OrderLineItems> orderLineItemsList) {
+        this.orderLineItemsList = orderLineItemsList;
+    }
 }
