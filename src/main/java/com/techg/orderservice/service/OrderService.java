7b94c6adf605a5d5220dab0ed483d1199ffec35b
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-08 14:03:24 +0300

    OrderService done

--- src/main/java/com/techg/orderservice/service/OrderService.java
+++ src/main/java/com/techg/orderservice/service/OrderService.java
@@ -1,2 +1,46 @@
-package com.techg.orderservice.service;public class OrderService {
+package com.techg.orderservice.service;
+
+import com.techg.orderservice.dto.OrderLineItemsDto;
+import com.techg.orderservice.dto.OrderRequestDto;
+import com.techg.orderservice.model.Order;
+import com.techg.orderservice.model.OrderLineItems;
+import com.techg.orderservice.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+//@RequiredArgsConstructor  This lombok annotation is an alternative of manually creating a constructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrder(OrderRequestDto orderRequestDto){
+        //creates an instance of new Order
+        Order order = new Order();
+        //set random order number
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequestDto.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+
+        //save to the repository
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
 }
