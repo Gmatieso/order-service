3cc4f88badbbe492d077ec7ed0c36f2ecb1442d7
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-08 13:52:56 +0300

    OrderLineitems model done

--- src/main/java/com/techg/orderservice/model/OrderLineItems.java
+++ src/main/java/com/techg/orderservice/model/OrderLineItems.java
@@ -11,8 +11,8 @@ import java.util.List;
 
 @Entity
 @Table(name="t_order_line_item")
-@Getter
-@Setter
+//@Getter
+//@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderLineItems {
@@ -21,8 +21,40 @@ public class OrderLineItems {
     private Long id;
 
     private String skuCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSkuCode() {
+        return skuCode;
+    }
+
+    public void setSkuCode(String skuCode) {
+        this.skuCode = skuCode;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
     private BigDecimal price;
     private Integer quantity;
-
 
 }
