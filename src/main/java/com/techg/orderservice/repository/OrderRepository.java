19c7a969fa692378d633386567e586293d5d1515
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-08 13:50:29 +0300

    OrderRepository done

--- src/main/java/com/techg/orderservice/repository/OrderRepository.java
+++ src/main/java/com/techg/orderservice/repository/OrderRepository.java
@@ -1,2 +1,8 @@
-package com.techg.orderservice.repository;public interface OrderRepository {
+package com.techg.orderservice.repository;
+
+import com.techg.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
