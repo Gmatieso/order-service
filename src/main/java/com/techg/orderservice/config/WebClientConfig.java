ad746bc23160d684a9576f9fb014ccd65aa6d88c
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-10 23:13:24 +0300

    WebClientConfig done

--- src/main/java/com/techg/orderservice/config/WebClientConfig.java
+++ src/main/java/com/techg/orderservice/config/WebClientConfig.java
@@ -1,2 +1,15 @@
-package com.techg.orderservice.config;public class WebClientConfig {
+package com.techg.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    //create a bean of type web client  and identify it with name webClient
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder().build();
+    }
 }
