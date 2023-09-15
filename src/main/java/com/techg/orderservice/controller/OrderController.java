83ddd36a92e46d45a61fb5fce5a13e08a711e56e
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-16 02:36:01 +0300

    Implementing retry mechanism

--- src/main/java/com/techg/orderservice/controller/OrderController.java
+++ src/main/java/com/techg/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.techg.orderservice.controller;
 import com.techg.orderservice.dto.OrderRequestDto;
 import com.techg.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")   //implementing circuit breaker logic
     @TimeLimiter(name = "inventory")
+    @Retry(name="inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequestDto orderRequestDto){
        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequestDto)) ;
     }
